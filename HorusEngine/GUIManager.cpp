d3743da3adf73cb7941a57944fea34fcd057b294
Marek Machliński <marekin450@spoko.pl>
2020-08-23 19:41:37 +0200

    Added default ImGui config file to be used in project.

--- HorusEngine/GUIManager.cpp
+++ HorusEngine/GUIManager.cpp
@@ -1,9 +1,13 @@
 #include "GUIManager.h"
+#include <filesystem>
 
 namespace GFX
 {
 	GUIManager::GUIManager()
 	{
+		if (!std::filesystem::exists("imgui.ini") && std::filesystem::exists("imgui_default.ini"))
+			std::filesystem::copy_file("imgui_default.ini", "imgui.ini");
+
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
 		ImGui::StyleColorsDark();
