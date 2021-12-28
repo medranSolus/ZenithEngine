ac8290db510a69b25b54af3b67fc10cc7c3dc4cd
Marek Machliński <marekin450@spoko.pl>
2021-12-28 22:00:00 +0100

    Changes FullscreenVS to generate triangle without input buffers.

--- Engine/Shader/VS/FullscreenVS.hlsl
+++ Engine/Shader/VS/FullscreenVS.hlsl
@@ -4,10 +4,19 @@ struct VSOut
 	float4 pos : SV_POSITION;
 };
 
-VSOut main(float2 pos : POSITION)
+VSOut main(uint id : SV_VertexID)
 {
 	VSOut vso;
-	vso.pos = float4(pos, 0.0f, 1.0f);
-	vso.tc = float2((pos.x + 1.0f) / 2.0f, (pos.y - 1.0f) / -2.0f);
+	/*
+	* X: 0,1 -> 0,  2  -> 2
+	* Y:  0  -> 2, 1,2 -> 0
+	*/
+	vso.tc = uint2(id & 2, ~((id & 1) ^ ((id >> 1) & 1)) << 1);
+	/*
+	* X: 0,1 -> -1,  2  -> 3
+	* Y:  0  -> -3, 1,2 -> 1
+	*/
+	const uint offset = vso.tc.x * 2;
+	vso.pos = float4(-1 + offset, -3 + offset, 0.0f, 1.0f);
 	return vso;
 }
