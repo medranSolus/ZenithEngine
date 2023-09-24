13037979ec61dca158c9efb12c43662e384d46d8
Marek Machliński <marekin450@spoko.pl>
2023-09-25 00:11:49 +0200

    Updated submodules and fixed compilation errors due to incorrect external projects configurations and missing file.

--- CMake/FidelityFX.cmake
+++ CMake/FidelityFX.cmake
@@ -17,17 +17,21 @@ file(GLOB FFXSDK_PUBLIC_SOURCES
     "${FFXSDK_HOST_PATH}/*.h")
     
 
-# Macro for loading FidelityFX SDK effects as external projects
+# Macro for adding FidelityFX SDK effects to project targets
 #	EFFECT_NAME = name of given effect
 # Returns link targets in variable FFXSDK_TARGETS
-macro(add_external_fidelityfx_effect EFFECT_NAME)
+macro(add_fidelityfx_target EFFECT_NAME)
     string(TOLOWER ${EFFECT_NAME} FFX${EFFECT_NAME})
 
     set(FFX${EFFECT_NAME}_DIR "${FFXSDK_DIR}/src/components/${FFX${EFFECT_NAME}}")
     set(FFX${EFFECT_NAME}_LIB "ffx_${FFX${EFFECT_NAME}}_${FFXSDK_PLATFORM_NAME}")
     set(FFX${EFFECT_NAME}_TARGET "${FFX${EFFECT_NAME}_LIB}")
     set(FFXSDK_TARGETS ${FFXSDK_TARGETS} ${FFX${EFFECT_NAME}_TARGET})
-    
+endmacro()
+
+# Macro for loading FidelityFX SDK effects as external projects
+#	EFFECT_NAME = name of given effect
+macro(add_external_fidelityfx_effect EFFECT_NAME)
     set(FFX${EFFECT_NAME}_CACHE_ARGS "-DFFX_${EFFECT_NAME}:BOOL=TRUE"
         "-DFFX_PLATFORM_NAME:STRING=${FFXSDK_PLATFORM_NAME}"
 	    "-DFFX_SHARED_SOURCES:STRING=${FFXSDK_SHARED_SOURCES}"
