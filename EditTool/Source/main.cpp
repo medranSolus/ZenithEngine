01961c41af620b780cfe336725731d204a751047
Marek Machliński <marekin450@spoko.pl>
2024-02-29 23:29:40 +0100

    Fixed edit tool to account for new surface handling.

--- EditTool/Source/main.cpp
+++ EditTool/Source/main.cpp
@@ -1,15 +1,10 @@
 #include "ScriptProcess.h"
-#include <combaseapi.h>
 
 int main(int argc, char* argv[])
 {
-	ZE_WIN_ENABLE_EXCEPT();
-
-	ZE_WIN_THROW_FAILED(CoInitializeEx(NULL, COINIT::COINIT_MULTITHREADED));
 	std::deque<std::string> input;
 	for (int i = 1; i < argc; ++i)
 		input.emplace_back(argv[i]);
 	const int code = ScriptProcess::Run(input);
-	CoUninitialize();
 	return code;
 }
