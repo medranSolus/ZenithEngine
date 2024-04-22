1f8e8e600bdf8f7121bed440278bfa2b70e75de1
Marek Machliński <marekin450@spoko.pl>
2024-04-22 23:03:12 +0200

    Removed proxy Generic resource.

--- Engine/Source/RHI/VK/Resource/Generic.cpp
+++
@@ -1,8 +0,0 @@
-#include "GFX/Resource/Generic.h"
-
-namespace ZE::RHI::VK::Resource
-{
-	void Generic::Copy(GFX::Device& dev, GFX::CommandList& cl, GFX::Resource::Generic& dest) const noexcept
-	{
-	}
-}
