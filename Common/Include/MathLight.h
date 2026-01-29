e98ef8e1c190d69f0d84e3ed1fcc6c0dd7ab572c
Marek Machliński <marekin450@spoko.pl>
2026-01-29 22:52:49 +0100

    Added PQ conversion to the math utilities.

--- Common/Include/MathLight.h
+++ Common/Include/MathLight.h
@@ -3,10 +3,34 @@
 
 namespace ZE::Math::Light
 {
+	// 1 in the linear frame-buffer space corresponds to this value of physical luminance [cd/m^2]
+	constexpr float REFERENCE_LUMINANCE = 100.0f;
+
 	inline float GetLightVolume(const ColorF3& color, float intensity, float attnLinear, float attnQuad) noexcept
 	{
 		const float lightMax = intensity * std::fmaxf(std::fmaxf(color.RGB.x, color.RGB.y), color.RGB.z);
 		return (-attnLinear + std::sqrtf(attnLinear * attnLinear - 4.0f * attnQuad * (1.0f - lightMax * 256.0f))) / (2.0f * attnQuad);
+	}
+
+	inline float ApplyPQ(float linearColor, float exponentScaleFactor) noexcept
+	{
+		constexpr float PQ_M1 = 2610.0f / 16384.0f;
+		constexpr float PQ_M2 = 2523.0f / 32.0f;
+		constexpr float PQ_C1 = 3424.0f / 4096.0f;
+		constexpr float PQ_C2 = 2413.0f / 128.0f;
+		constexpr float PQ_C3 = 2392.0f / 128.0f;
+		constexpr float PQ_PQC = 10000.0f; // Max supported luminance
+
+		linearColor *= REFERENCE_LUMINANCE;
+		if (linearColor < 0.0f)
+			linearColor = 0.0f;
+		else if (linearColor > PQ_PQC)
+			linearColor = 1.0f;
+		else
+			linearColor /= PQ_PQC;
+
+		float y = std::powf(linearColor, PQ_M1);
+		return std::exp2f(exponentScaleFactor * PQ_M2 * (std::log2f(PQ_C1 + PQ_C2 * y) - std::log2f(1.0f + PQ_C3 * y)));
 	}
 
 	constexpr void SetLightAttenuation(float& linear, float& quad, U64 range) noexcept
